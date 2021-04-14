6de2641f0d1470e39d1b3f9f334df29caa31ec81
Florian Benedetti <florian.benedetti@cea.fr>
2021-04-14 11:43:25 +0200

    Add input data function

---
+++ emva1288/common/input.py
@@ -0,0 +1,32 @@
+import h5py
+import numpy as np
+import logging
+import sys
+
+DATA_DIR = '/local/home/fb250757/Documents/Data/Camera/'
+
+
+def load_hdf5(filename):
+    """Load data images from an HDF5 file.
+
+    Args:
+        filename (string): Full path of the HDF5 file.
+
+    Returns:
+        (ndarray): Images data.
+    """
+    try:
+        logging.info('Try to open file {}'.format(filename))
+        file = h5py.File(filename, 'r')
+        logging.info('File open: done')
+        data = file['entry/data/data'][:]
+        timestamp = file['entry/instrument/NDAttributes/NDArrayTimeStamp'][:]
+        time_s = file['entry/instrument/NDAttributes/NDArrayEpicsTSSec'][:]
+        time_ns = file['entry/instrument/NDAttributes/NDArrayEpicsTSnSec'][:]
+        logging.info('Read data: done')
+        file.close()
+        logging.info('File is close '.format(filename))
+        return data, timestamp, time_s, time_ns
+    except (OSError):
+        logging.exception('Unable to open file')
+        sys.exit(1)
